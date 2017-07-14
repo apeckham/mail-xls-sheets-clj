08c8c9196c03cd7686a05c9acaace67a4f625c62
apeckham <apeckham@gmail.com>
2017-07-13 23:10:44 -0700

    add notes on reading excel files

--- project.clj
+++ project.clj
@@ -6,7 +6,7 @@
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [google-apps-clj "0.6.1"]
                  [com.sun.mail/javax.mail "1.6.0-rc2"]
-                 [org.apache.poi/poi "3.16"]]
+                 [org.apache.poi/poi-ooxml "3.16"]]
   :main ^:skip-aot mail-xls-sheets-clj.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
